ff742518cf7ba5490e5667b7f18217e260d695cd
Matt Greer <matt.e.greer@gmail.com>
2021-12-30 11:30:32 -0500

    tests.c: remove comments

--- src/tests.c
+++ src/tests.c
@@ -14,20 +14,12 @@ void basicScale(s16 x, s16 y, s16 scale) {
         *REG_VRAMRW = (palette << 8);
     }
 
-    // append tile zero to the end, which is blank
-    // requires sprites never be taller than 30 tiles
-    *REG_VRAMRW = 0;
-    *REG_VRAMRW = 0;
-
     *REG_VRAMMOD = SCB234_SIZE;
     *REG_VRAMADDR = ADDR_SCB2 + spriteIndex;
 
     *REG_VRAMRW = scale;
 
-    // TODO: if the sprite is sticky, x and y don't matter, should we skip them?
-    // y position, height in tiles, whether it is sticky
     *REG_VRAMRW = (TO_SCREEN_Y(y) << 7) | height;
-    // x position
     *REG_VRAMRW = TO_SCREEN_X(x) << 7;
 
     ++spriteIndex;
@@ -51,20 +43,12 @@ void basicScaleTileZeroToEnd(s16 x, s16 y, s16 scale) {
         *REG_VRAMRW = (palette << 8);
     }
 
-    // append tile zero to the end, which is blank
-    // requires sprites never be taller than 30 tiles
-    *REG_VRAMRW = 0;
-    *REG_VRAMRW = 0;
-
     *REG_VRAMMOD = SCB234_SIZE;
     *REG_VRAMADDR = ADDR_SCB2 + spriteIndex;
 
     *REG_VRAMRW = scale;
 
-    // TODO: if the sprite is sticky, x and y don't matter, should we skip them?
-    // y position, height in tiles, whether it is sticky
     *REG_VRAMRW = (TO_SCREEN_Y(y) << 7) | height;
-    // x position
     *REG_VRAMRW = TO_SCREEN_X(x) << 7;
 
     ++spriteIndex;
@@ -85,20 +69,12 @@ void withTransparentBottomRow(s16 x, s16 y, s16 scale) {
     *REG_VRAMRW = 11;
     *REG_VRAMRW = (palette << 8);
 
-    // append tile zero to the end, which is blank
-    // requires sprites never be taller than 30 tiles
-    *REG_VRAMRW = 0;
-    *REG_VRAMRW = 0;
-
     *REG_VRAMMOD = SCB234_SIZE;
     *REG_VRAMADDR = ADDR_SCB2 + spriteIndex;
 
     *REG_VRAMRW = scale;
 
-    // TODO: if the sprite is sticky, x and y don't matter, should we skip them?
-    // y position, height in tiles, whether it is sticky
     *REG_VRAMRW = (TO_SCREEN_Y(y) << 7) | (height + 1);
-    // x position
     *REG_VRAMRW = TO_SCREEN_X(x) << 7;
 
     ++spriteIndex;
@@ -126,20 +102,12 @@ void withTransparentBottomRowTileZeroToEnd(s16 x, s16 y, s16 scale) {
         *REG_VRAMRW = (palette << 8);
     }
 
-    // append tile zero to the end, which is blank
-    // requires sprites never be taller than 30 tiles
-    *REG_VRAMRW = 0;
-    *REG_VRAMRW = 0;
-
     *REG_VRAMMOD = SCB234_SIZE;
     *REG_VRAMADDR = ADDR_SCB2 + spriteIndex;
 
     *REG_VRAMRW = scale;
 
-    // TODO: if the sprite is sticky, x and y don't matter, should we skip them?
-    // y position, height in tiles, whether it is sticky
     *REG_VRAMRW = (TO_SCREEN_Y(y) << 7) | height;
-    // x position
     *REG_VRAMRW = TO_SCREEN_X(x) << 7;
 
     ++spriteIndex;
